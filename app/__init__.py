510d17645a1b39de3464de5b57cd88b54c700543
Collapsium <cristian.rodriguez_b@mail.udp.cl>
2022-12-27 00:12:18 -0300

    add header and card macro

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 #lo que traeremos a flask, por el nombre python cacha que es un package
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 from app import views
 from app import admin_views
--- app/views.py
+++ app/views.py
@@ -5,6 +5,37 @@ from flask import render_template
 def index():  #Funcion que anda cuando se está en "/"
     return render_template('/public/index.html')
 
-@app.route("/pagina2")
-def pagina2():
-    return "<h1 style='color:blue' > pagina2 </h1>"
+@app.route("/data")
+def data():
+    dato = "amongus" #Puede ser cualquier objeto de python
+    arr_data = ["dato1 de arr", "dato2 de arr", "dato3 de arr"]
+    dict_data = {"dato1 de dictionary": 1, "dato2 de dictionary": 2, "dato3 de dictionary": 3}
+    pool_data = ("pool1", "pool2")
+
+    def funcion(x, qty):
+        return x * qty
+
+    class clase_epica:
+        def __init__(self, arg):
+            self.arg = arg
+
+        def test(self):
+            return f"funcionando clase_epica, {self.arg}"
+
+    my_clase_epica = clase_epica(
+        arg = "amongus"
+    )
+
+    return render_template(
+        '/public/data.html',
+        dato=dato,
+        arr_data = arr_data,
+        dict_data = dict_data,
+        pool_data = pool_data,
+        clase_epica = clase_epica,
+        my_clase_epica = my_clase_epica
+    )
+
+@app.route("/recommendations")
+def recommendations():
+    return render_template('/public/recommendations.html')
